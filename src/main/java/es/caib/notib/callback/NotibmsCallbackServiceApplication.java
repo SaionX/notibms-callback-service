09614efc145b906fee1e4fda0711a5f60dd912ac
sion <sion.andreu@gmail.com>
2021-01-17 19:07:52 +0100

    Pujada inicial

---
+++ src/main/java/es/caib/notib/callback/NotibmsCallbackServiceApplication.java
@@ -0,0 +1,13 @@
+package es.caib.notib.callback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotibmsCallbackServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotibmsCallbackServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/caib/notib/callback/NotibmsCallbackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.caib.notib.callback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotibmsCallbackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
